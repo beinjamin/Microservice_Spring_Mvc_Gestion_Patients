ec3cc861ee112634270be57a2cf42fcdc4731345
beinjamin <jauresbeinjamin@gmail.com>
2022-04-16 21:06:24 +0100

    Initialisation

---
+++ src/main/java/com/jaures/patientsmvc/PatientsMvcApplication.java
@@ -0,0 +1,13 @@
+package com.jaures.patientsmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsMvcApplication.class, args);
+	}
+
+}
