8fe422f9de94a9f750b91372a527a8e7a2d04a74
beinjamin <jauresbeinjamin@gmail.com>
2022-04-18 10:45:16 +0100

    Annotation @Bean pour que cette methode execute automatiquement

--- src/main/java/com/jaures/patientsmvc/PatientsMvcApplication.java
+++ src/main/java/com/jaures/patientsmvc/PatientsMvcApplication.java
@@ -1,7 +1,9 @@
 package com.jaures.patientsmvc;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PatientsMvcApplication {
@@ -9,5 +11,11 @@ public class PatientsMvcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PatientsMvcApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner commandLineRunner()
+	{
+		return args -> {
+			
+		};
+	}
 }
