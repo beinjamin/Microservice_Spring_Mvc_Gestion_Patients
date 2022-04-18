85c92e613754d4e0a0f8c6126db760bb1846d080
beinjamin <jauresbeinjamin@gmail.com>
2022-04-18 10:40:12 +0100

    Finalisation avec class patient

--- src/main/java/entities/Patient.java
+++ src/main/java/entities/Patient.java
@@ -3,14 +3,22 @@ package entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 @Entity
-@Data @AllArgsConstructor 
+@Data @AllArgsConstructor @NoArgsConstructor
 public class Patient {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nom;
+	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
 	private boolean malade;
 	private int score;
