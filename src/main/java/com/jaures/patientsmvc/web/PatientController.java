de0b97168d26793947f13b497a4869e61186f915
beinjamin <jauresbeinjamin@gmail.com>
2022-04-18 11:14:09 +0100

    Ajout d'un constructeur avec parametre

--- src/main/java/com/jaures/patientsmvc/web/PatientController.java
+++ src/main/java/com/jaures/patientsmvc/web/PatientController.java
@@ -1,5 +1,13 @@
 package com.jaures.patientsmvc.web;
 
+import com.jaures.patientsmvc.repositories.PatientRepository;
+
 public class PatientController {
+	
+	private PatientRepository patientRepository;
+	
+	public PatientController(PatientRepository patientRepository) {
+		this.patientRepository = patientRepository;
+	}
 
 }
