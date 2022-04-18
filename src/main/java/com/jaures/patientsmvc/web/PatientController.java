4f3e42421f73a965c5b60d819c42dc0ca0b24c8a
beinjamin <jauresbeinjamin@gmail.com>
2022-04-18 11:17:27 +0100

    Creation de la methode pour afficher le vue

--- src/main/java/com/jaures/patientsmvc/web/PatientController.java
+++ src/main/java/com/jaures/patientsmvc/web/PatientController.java
@@ -1,6 +1,7 @@
 package com.jaures.patientsmvc.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.jaures.patientsmvc.repositories.PatientRepository;
 
@@ -12,6 +13,9 @@ public class PatientController {
 	
 	private PatientRepository patientRepository;
 	
-	
-
+	@GetMapping(path = "/index")
+    public String patients() {
+		return "patients";
+    	
+    }
 }
