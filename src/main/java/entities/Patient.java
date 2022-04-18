d2a74a79636321730933c5817c087c3dfbbae2f8
beinjamin <jauresbeinjamin@gmail.com>
2022-04-18 10:35:48 +0100

    mise en place des annotation jpa

--- src/main/java/entities/Patient.java
+++ src/main/java/entities/Patient.java
@@ -1,5 +1,18 @@
 package entities;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Entity
+@Data @AllArgsConstructor 
 public class Patient {
+	private Long id;
+	private String nom;
+	private Date dateNaissance;
+	private boolean malade;
+	private int score;
 
 }
