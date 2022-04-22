06933f515ab7866f7151c5982e5a14c1059d4e8a
beinjamin <jauresbeinjamin@gmail.com>
2022-04-22 11:32:14 +0100

    Insertion de la pagination

--- src/main/java/com/jaures/patientsmvc/web/PatientController.java
+++ src/main/java/com/jaures/patientsmvc/web/PatientController.java
@@ -2,9 +2,12 @@ package com.jaures.patientsmvc.web;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jaures.patientsmvc.entities.Patient;
 import com.jaures.patientsmvc.repositories.PatientRepository;
@@ -19,9 +22,11 @@ public class PatientController {
 	private PatientRepository patientRepository;
 	
 	@GetMapping(path = "/index")
-    public String patients(Model model, int page , int size) {
-		List<Patient> patients=patientRepository.findAll();
-		model.addAttribute("listPatients",patients);
+    public String patients(Model model, 
+    		@RequestParam(name = "page",defaultValue = "0" )int page ,
+    		@RequestParam(name = "size",defaultValue = "0" )int size) {
+		Page<Patient> patients=patientRepository.findAll(PageRequest.of(page, size));
+		model.addAttribute("listPatients",patients.getContent());
 		return "patients";
     	
     }
